dace6a90436af5bccdef06468b79d32e5a78d891
Paul Scarpa <spryh@Bootstrap.home>
2017-12-19 16:06:25 -0800

    thru 2.3 - Nested components, barrel importing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser' // check
 import { NgModule } from '@angular/core' // check
 import { AppComponent } from './app.component' // check
+// import { EntryListComponent } from './entries/entry-list/entry-list.component' but let's use a barrel by adding entries/index.ts
+import { EntryListComponent, EntryComponent } from './entries'
 
 /* Decorator post processing - maps components in the build
    Declarations registers the component, avoids "custom undefined HTML"
    BrowserModule makes the app available in the browser.
    Bootstrap starts AppComponent at launch
 */
+
+// Always put child components before parents in declarations!
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryComponent,
+    EntryListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/entries/entry/entry.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component ({
+    selector: 'app-entry', // kabobcase is html specfication
+    templateUrl: 'entry.component.html',
+    styleUrls: ['entry.component.css']
+})
+
+export class EntryComponent {
+
+}
